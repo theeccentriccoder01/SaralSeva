f9d9cb7321d2bf95f873c19c0fad89ceb1c6326d
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-10-10 03:09:14 +0530

    Update rateLimiterConfig.js

--- backend/middleware/rateLimiting/rateLimiterConfig.js
+++ backend/middleware/rateLimiting/rateLimiterConfig.js
@@ -1,5 +1,14 @@
 module.exports = {
-    strict: { points: 5, duration: 60 },
-    moderate: { points: 30, duration: 60 },
-    light: { points: 100, duration: 60 },
+  strict: {
+    points: 5,
+    duration: 60,
+  },
+  moderate: {
+    points: 30,
+    duration: 60,
+  },
+  light: {
+    points: 100,
+    duration: 60,
+  },
 };
