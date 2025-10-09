f28e2a51539e758bf260d4f2763e8d921ff3bb75
theeccentriccoder01 <ceo@edulinkup.dev>
2025-10-09 15:01:01 +0530

    Update

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import "./App.css";
