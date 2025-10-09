e3ed8271dcc77012953aa5a8cd3f7942f91d4646
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-10-10 03:08:13 +0530

    Update rateLimiter.js

--- backend/middleware/rateLimiting/rateLimiter.js
+++ backend/middleware/rateLimiting/rateLimiter.js
@@ -1,16 +1,19 @@
 import { RateLimiterMemory } from 'rate-limiter-flexible';
-import config from './rateLimiterConfig';
+import config from './rateLimiterConfig.js';
 
 export const createRateLimiter = (level) => {
+  const { points, duration } = config[level] || config.light;
 
-    const { points, duration } = config[level] || config.light;
-    const limiter = new RateLimiterMemory({ points, duration });
+  const limiter = new RateLimiterMemory({ points, duration });
 
-    return (req, res, next) => {
-        limiter.consume(req.ip).then(() => next()).catch(() => {
-            res.status(429).json({
-                message: 'Too Many Requests. Please try again later.'
-            });
-        })
-    }
-}
+  return (req, res, next) => {
+    limiter
+      .consume(req.ip)
+      .then(() => next())
+      .catch(() => {
+        res.status(429).json({
+          message: 'Too Many Requests. Please try again later.',
+        });
+      });
+  };
+};
