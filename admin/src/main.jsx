9768952262403724268280158a56a7947b387fe4
Priyanka Chavan <priyankachavan220406@gmail.com>
2025-12-15 02:58:38 +0530

    added basic offline support in admin part

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AdminProvider } from "./components/context/adminContext.jsx";
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,3 +16,17 @@ createRoot(document.getElementById("root")).render(
     </HelmetProvider>
   </StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered:", registration);
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
+
